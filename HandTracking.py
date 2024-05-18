fd828bbc3e05756fb66b23ff1dfdd866669145c6
Zishan <mrzishan12345@gmail.com>
2024-05-18 17:17:30 +0530

    edited the files and added a new showFpsOnScreen() function in HandTrackingModule.py file

--- HandTracking.py
+++ HandTracking.py
@@ -11,12 +11,12 @@ currentTime=0
 previousTime=0
 while True:
     res, frame = cap.read()
-    handTracker.detectHands(frame)
-    handTracker.findPosition(frame, landmarkId=4)
-    currentTime = time.time()
-    fps = 1/(currentTime-previousTime)
-    previousTime = currentTime
     
-    cv.putText(frame, str(int(fps)), (15,50), cv.FONT_HERSHEY_SIMPLEX, 2, (255,0,255), 2)
+    frame = handTracker.detectHands(frame, draw=False)
+    frame = handTracker.showFpsOnScreen(frame)
+    handTracker.findPosition(frame, landmarkId=4)
+
+    
+    #cv.putText(frame, str(int(fps)), (15,50), cv.FONT_HERSHEY_SIMPLEX, 2, (255,0,255), 2)
     cv.imshow("Video",frame)
     cv.waitKey(1)
--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -8,6 +8,9 @@ class handDetector():
         self.maxNoOfHands = maxNoOfHands
         self.detectConfidence = detectConfidence
         self.trackConfidence = trackConfidence
+        self.previousTime = 0
+        self.currentTime = 0
+        self.fps = 0
         
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(static_image_mode=self.mode,
@@ -39,29 +42,37 @@ class handDetector():
                     cv.circle(frame, (xPos, yPos), 10, (210, 100, 140), cv.FILLED)
             
         return landmarkList
-
-def main():
-    cap = cv.VideoCapture(0)
-    previousTime = 0
-    currentTime = 0
     
-    detector = handDetector()
-    
-    while True:
-        res, frame = cap.read()
-        frame = detector.detectHands(frame)
-        landmarkList = detector.findPosition(frame, landmarkId=8)
-        
-        if len(landmarkList) != 0:
-            print(landmarkList[8])
-        
-        currentTime = time.time()
-        fps = 1 / (currentTime - previousTime)
-        previousTime = currentTime
-        
-        cv.putText(frame, str(int(fps)), (15, 50), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 2)
-        cv.imshow("Video", frame)
-        cv.waitKey(1)
+    def showFpsOnScreen(self, frame):
+        self.currentTime = time.time()
+        self.fps = 1 / (self.currentTime - self.previousTime)
+        self.previousTime = self.currentTime
+        cv.putText(frame, str(int(self.fps)), (15, 50), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 2)
+        return frame
 
-if __name__ == "__main__":
-    main()
+
+
+
+#this is just a usage example like how can we use this module in different files
+# def main():
+#     cap = cv.VideoCapture(0)
+    
+    
+#     detector = handDetector()
+    
+#     while True:
+#         res, frame = cap.read()
+#         frame = detector.detectHands(frame)
+#         landmarkList = detector.findPosition(frame, landmarkId=8)
+#         frame = detector.showFpsOnScreen(frame)
+#         if len(landmarkList) != 0:
+#             print(landmarkList[8])
+        
+        
+        
+        
+#         cv.imshow("Video", frame)
+#         cv.waitKey(1)
+
+# if __name__ == "__main__":
+#     main()
